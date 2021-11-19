088a7d4fa6a1bcbafa31a2515b4a8beeb87185c5
phihash <insharp0220@gmail.com>
2021-11-19 12:39:36 +0900

    微小な変更

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const answers = {
   "DM":"糖尿病",
   "AD":"アルツハイマー病 あるいは 常染色体優性遺伝",
   "AAA":"腹部大動脈瘤",
+  "VF":"心室細動",
+  "AF":"心房細動",
   "MS":"多発性硬化症 あるいは 僧帽弁狭窄症",
   "AA":"再生不良性貧血",
   "PBC":"原発性胆汁性胆管炎",
@@ -85,7 +87,7 @@ function processAlphabets(string){
 
 searchButton.addEventListener('click',() => {
   if(checkIsAlphabets(searchValue.value)){
-    confirm("半角英字で入力してください");
+    confirm("半角英字のみで入力してください");
     return;
   }
   let text = processAlphabets(searchValue.value);
@@ -108,7 +110,7 @@ searchButton.addEventListener('click',() => {
 searchValue.addEventListener('keypress',(e) => {
   if(e.key == 'Enter'){
     if(checkIsAlphabets(searchValue.value)){
-      confirm("半角英字で入力してください");
+      confirm("半角英字のみで入力してください");
       return;
     }
     let text = processAlphabets(searchValue.value);
